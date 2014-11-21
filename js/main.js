4d86bd31f9f4dc8b1b416bd02fbadfda8a964bdb
Toum <thoumsinf@gmail.com>
2014-11-21 23:53:42 +0100

    n1

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ function onLoad() {
 //ON DEVICE READY
 function onDeviceReady() {
     pushNotification = window.plugins.pushNotification;
+    registering();
 }
 
 
@@ -93,39 +94,40 @@ function swiperightHandler( event ){
 
 
 //NOTIFICATION PLUGIN REGISTER
-if ( device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos" ){
-    pushNotification.register(
-    successHandler,
-    errorHandler,
-    {
-        "senderID":"replace_with_sender_id",
-        "ecb":"onNotification"
-    });
-} else if ( device.platform == 'blackberry10'){
-    pushNotification.register(
-    successHandler,
-    errorHandler,
-    {
-        invokeTargetId : "replace_with_invoke_target_id",
-        appId: "replace_with_app_id",
-        ppgUrl:"replace_with_ppg_url", //remove for BES pushes
-        ecb: "pushNotificationHandler",
-        simChangeCallback: replace_with_simChange_callback,
-        pushTransportReadyCallback: replace_with_pushTransportReady_callback,
-        launchApplicationOnPush: true
-    });
-} else {
-    pushNotification.register(
-    tokenHandler,
-    errorHandler,
-    {
-        "badge":"true",
-        "sound":"true",
-        "alert":"true",
-        "ecb":"onNotificationAPN"
-    });
+function registering(){
+	if ( device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos" ){
+	    pushNotification.register(
+	    successHandler,
+	    errorHandler,
+	    {
+	        "senderID":"replace_with_sender_id",
+	        "ecb":"onNotification"
+	    });
+	} else if ( device.platform == 'blackberry10'){
+	    pushNotification.register(
+	    successHandler,
+	    errorHandler,
+	    {
+	        invokeTargetId : "replace_with_invoke_target_id",
+	        appId: "replace_with_app_id",
+	        ppgUrl:"replace_with_ppg_url", //remove for BES pushes
+	        ecb: "pushNotificationHandler",
+	        simChangeCallback: replace_with_simChange_callback,
+	        pushTransportReadyCallback: replace_with_pushTransportReady_callback,
+	        launchApplicationOnPush: true
+	    });
+	} else {
+	    pushNotification.register(
+	    tokenHandler,
+	    errorHandler,
+	    {
+	        "badge":"true",
+	        "sound":"true",
+	        "alert":"true",
+	        "ecb":"onNotificationAPN"
+	    });
+	}
 }
-
 
 //NOTIFICATION HANDLERS
 function successHandler (result) {
