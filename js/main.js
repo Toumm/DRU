4f55a9d725f8620fda060647b275dba11938205a
Toum <thoumsinf@gmail.com>
2014-11-21 17:41:59 +0100

    hover

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ function onLoad() {
 }
 
 function onDeviceReady() {
-    console.log("device is ready!");
+    alert("device is ready!");
+    $("#dealcta").click(function(){
+	    var hover = $(".dealHover");
+	    var w = $(document);
+	    hover.width(w.width()*0.8);
+	    hover.height(w.width()*0.8);
+	    hover.css("top", w.height()/4);
+	    hover.css("left", w.width()*0.1);
+	    hover.html('<i class="fa fa-check green" style="font-size: '+ w.width()*0.6 +'px;"></i>');
+	    hover.fadeIn();
+	    setTimeout(function(){ hover.fadeOut(); }, 2500);
+    });
 }
