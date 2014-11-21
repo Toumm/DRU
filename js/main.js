74b816050f1627733347979f3fec0c2186d41a46
Toum <thoumsinf@gmail.com>
2014-11-21 21:04:58 +0100

    bg

--- js/main.js
+++ js/main.js
@@ -10,27 +10,47 @@ function onDeviceReady() {
 }
     
 function swipeleftHandler( event ){
-    var hover = $(".dealHover");
-    var w = $(document);
-    hover.width(w.width()*0.8);
-    hover.height(w.width()*0.8);
-    var h = hover.height();
-    hover.css("top", w.height()/4);
-    hover.css("left", w.width()*0.1);
-    hover.html('<img src="img/Check.png" height="'+h*0.5+'px" width="'+h*0.5+'px" style="margin: 40px;" /><div class="green" style="font-size:'+w.width()*0.1+'px;">DEAL!</div>');
-    hover.fadeIn();
-    setTimeout(function(){ hover.fadeOut(); }, 2500);
+    elem = $(event.target);
+     if(!elem.hasClass("dare")){
+	    elem = elem.parent("div.dare");
+	}
+    if(elem.hasClass("refused")){
+	    elem.removeClass("refused");
+    }
+    else{
+	    var hover = $(".dealHover");
+	    var w = $(document);
+	    hover.width(w.width()*0.8);
+	    hover.height(w.width()*0.8);
+	    var h = hover.height();
+	    hover.css("top", w.height()/4);
+	    hover.css("left", w.width()*0.1);
+	    hover.html('<img src="img/Check.png" height="'+h*0.5+'px" width="'+h*0.5+'px" style="margin: 40px;" /><div class="green" style="font-size:'+w.width()*0.1+'px;">DEAL!</div>');
+	    hover.fadeIn();
+	    setTimeout(function(){ hover.fadeOut(); }, 2500);
+	    elem.addClass("accepted");
+    }
 }
     
 function swiperightHandler( event ){
-    var hover = $(".dealHover");
-    var w = $(document);
-    hover.width(w.width()*0.8);
-    hover.height(w.width()*0.8);
-    var h = hover.height();
-    hover.css("top", w.height()/4);
-    hover.css("left", w.width()*0.1);
-    hover.html('<img src="img/noway.png" height="'+h*0.4+'px" width="'+h*0.4+'px" style="margin: 40px;" /><div class="red" style="font-size:'+w.width()*0.1+'px;">NO WAY!</div>');
-    hover.fadeIn();
-    setTimeout(function(){ hover.fadeOut(); }, 2500);
+    elem = $(event.target);
+     if(!elem.hasClass("dare")){
+	    elem = elem.parent("div.dare");
+	}
+    if(elem.hasClass("accepted")){
+	    elem.removeClass("accepted");
+    }
+    else{
+	    var hover = $(".dealHover");
+	    var w = $(document);
+	    hover.width(w.width()*0.8);
+	    hover.height(w.width()*0.8);
+	    var h = hover.height();
+	    hover.css("top", w.height()/4);
+	    hover.css("left", w.width()*0.1);
+	    hover.html('<img src="img/noway.png" height="'+h*0.4+'px" width="'+h*0.4+'px" style="margin: 40px;" /><div class="red" style="font-size:'+w.width()*0.1+'px;">NO WAY!</div>');
+	    hover.fadeIn();
+	    elem.addClass("refused");
+	    setTimeout(function(){ hover.fadeOut(); }, 2500);
+    }
 }
