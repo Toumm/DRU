1dc9a3090d20827e18058103c7e260d8ed2716d1
Toum <thoumsinf@gmail.com>
2014-11-21 18:01:24 +0100

    hove pngs

--- js/main.js
+++ js/main.js
@@ -11,7 +11,18 @@ function onDeviceReady() {
 	    hover.height(w.width()*0.8);
 	    hover.css("top", w.height()/4);
 	    hover.css("left", w.width()*0.1);
-	    hover.html('<i class="fa fa-check green" style="font-size: '+ w.width()*0.6 +'px;"></i>');
+	    hover.html('<img src="img/Check.png" style="margin: 40px;" /><div class="green" style="font-size:'+w.width()*0.1+'px;">DEAL!</div>');
+	    hover.fadeIn();
+	    setTimeout(function(){ hover.fadeOut(); }, 2500);
+    });
+    $("#nowaycta").click(function(){
+	    var hover = $(".dealHover");
+	    var w = $(document);
+	    hover.width(w.width()*0.8);
+	    hover.height(w.width()*0.8);
+	    hover.css("top", w.height()/4);
+	    hover.css("left", w.width()*0.1);
+	    hover.html('<img src="img/noway.png" style="margin: 40px;" /><div class="red" style="font-size:'+w.width()*0.1+'px;">NO WAY!</div>');
 	    hover.fadeIn();
 	    setTimeout(function(){ hover.fadeOut(); }, 2500);
     });
