6b24948a7576c6519b17f143178c3493038506c1
Toum <thoumsinf@gmail.com>
2014-11-22 00:15:13 +0100

    dready

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ function onLoad() {
 
 //ON DEVICE READY
 function onDeviceReady() {
+	alert("appReady");
     pushNotification = window.plugins.pushNotification;
     registering();
 }
@@ -95,6 +96,7 @@ function swiperightHandler( event ){
 
 //NOTIFICATION PLUGIN REGISTER
 function registering(){
+	alert("registering...");
 	if ( device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos" ){
 		alert("registering1");
 	    pushNotification.register(
@@ -104,22 +106,8 @@ function registering(){
 	        "senderID":"replace_with_sender_id",
 	        "ecb":"onNotification"
 	    });
-	} else if ( device.platform == 'blackberry10'){
-		alert("registering2");
-	    pushNotification.register(
-	    successHandler,
-	    errorHandler,
-	    {
-	        invokeTargetId : "replace_with_invoke_target_id",
-	        appId: "replace_with_app_id",
-	        ppgUrl:"replace_with_ppg_url", //remove for BES pushes
-	        ecb: "pushNotificationHandler",
-	        simChangeCallback: replace_with_simChange_callback,
-	        pushTransportReadyCallback: replace_with_pushTransportReady_callback,
-	        launchApplicationOnPush: true
-	    });
 	} else {
-	
+		alert("registering2");
 	    pushNotification.register(
 	    tokenHandler,
 	    errorHandler,
