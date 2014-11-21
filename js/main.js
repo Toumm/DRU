485a41c6945228ad1712cdea7c75cfd4921c3d03
Toum <thoumsinf@gmail.com>
2014-11-21 20:40:14 +0100

    i m a jerk

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function onLoad() {
     document.addEventListener("deviceready", onDeviceReady, false);
     $( "div.dare" ).on( "swipeleft", swipeleftHandler );
+    $( "div.dare" ).on( "swiperight", swiperightHandler );
 
 }
 
@@ -9,31 +10,27 @@ function onDeviceReady() {
 }
     
 function swipeleftHandler( event ){
-    $("#dealcta").click(function(){
-	    var hover = $(".dealHover");
-	    var w = $(document);
-	    hover.width(w.width()*0.8);
-	    hover.height(w.width()*0.8);
-	    var h = hover.height();
-	    hover.css("top", w.height()/4);
-	    hover.css("left", w.width()*0.1);
-	    hover.html('<img src="img/Check.png" height="'+h*0.5+'px" width="'+h*0.5+'px" style="margin: 40px;" /><div class="green" style="font-size:'+w.width()*0.1+'px;">DEAL!</div>');
-	    hover.fadeIn();
-	    setTimeout(function(){ hover.fadeOut(); }, 2500);
-    });
+    var hover = $(".dealHover");
+    var w = $(document);
+    hover.width(w.width()*0.8);
+    hover.height(w.width()*0.8);
+    var h = hover.height();
+    hover.css("top", w.height()/4);
+    hover.css("left", w.width()*0.1);
+    hover.html('<img src="img/Check.png" height="'+h*0.5+'px" width="'+h*0.5+'px" style="margin: 40px;" /><div class="green" style="font-size:'+w.width()*0.1+'px;">DEAL!</div>');
+    hover.fadeIn();
+    setTimeout(function(){ hover.fadeOut(); }, 2500);
 }
     
 function swiperightHandler( event ){
-    $("#nowaycta").click(function(){
-	    var hover = $(".dealHover");
-	    var w = $(document);
-	    hover.width(w.width()*0.8);
-	    hover.height(w.width()*0.8);
-	    var h = hover.height();
-	    hover.css("top", w.height()/4);
-	    hover.css("left", w.width()*0.1);
-	    hover.html('<img src="img/noway.png" height="'+h*0.4+'px" width="'+h*0.4+'px" style="margin: 40px;" /><div class="red" style="font-size:'+w.width()*0.1+'px;">NO WAY!</div>');
-	    hover.fadeIn();
-	    setTimeout(function(){ hover.fadeOut(); }, 2500);
-    });
+    var hover = $(".dealHover");
+    var w = $(document);
+    hover.width(w.width()*0.8);
+    hover.height(w.width()*0.8);
+    var h = hover.height();
+    hover.css("top", w.height()/4);
+    hover.css("left", w.width()*0.1);
+    hover.html('<img src="img/noway.png" height="'+h*0.4+'px" width="'+h*0.4+'px" style="margin: 40px;" /><div class="red" style="font-size:'+w.width()*0.1+'px;">NO WAY!</div>');
+    hover.fadeIn();
+    setTimeout(function(){ hover.fadeOut(); }, 2500);
 }
